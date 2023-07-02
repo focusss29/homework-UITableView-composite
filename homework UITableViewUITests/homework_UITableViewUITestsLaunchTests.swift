87a7f12563b22175ce8d3b45eaf9b68ef266f922
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-07-03 01:00:36 +0600

    Initial Commit

---
+++ homework UITableViewUITests/homework_UITableViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  homework_UITableViewUITestsLaunchTests.swift
+//  homework UITableViewUITests
+//
+//  Created by Ilya Pogozhev on 03.07.2023.
+//
+
+import XCTest
+
+final class homework_UITableViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
